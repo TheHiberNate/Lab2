3e2eea3951fdef4f6854699714045660406f71b0
TheHiberNate <nathan@gawargy.com>
2023-01-31 14:21:30 -0500

    Updates

--- TrafficLightsSimulator/src/SEG2106/core/Main.java
+++ TrafficLightsSimulator/src/SEG2106/core/Main.java
@@ -6,8 +6,13 @@ public class Main {
 		// get the singleton object of the traffic light manager
 		TrafficLightManager trafficLightManager = TrafficLightManager.getTrafficManager();
 		
+		String trafficCondition = "none";
+
+		if (args.length > 0) {
+			trafficCondition = args[0];
+		} 
 		// Create the traffic light state machine
-		new TrafficLight(trafficLightManager);
+		new TrafficLight(trafficLightManager, trafficCondition);
 		
 		// Set the traffic condition
 		if (args.length > 0){
--- TrafficLightsSimulator/src/SEG2106/core/TrafficLight.java
+++ TrafficLightsSimulator/src/SEG2106/core/TrafficLight.java
@@ -22,11 +22,11 @@ public class TrafficLight implements EventHandler {
 
   // TrafficLight State Machines
   public enum Status {
-    // default 
+    // default
     northAndSouthGreen, northAndSouthYellow, northAndSouthRed, westAndEastYellow,
-    
+
     // lightTraffic
-    northAndSouthGreenAndArrowLight, northAndSouthGreenLight, northAndSouthYellowLight, 
+    northAndSouthGreenAndArrowLight, northAndSouthGreenLight, northAndSouthYellowLight,
     northAndSouthRedLight, westAndEastYellowLight
   }
 
@@ -36,10 +36,12 @@ public class TrafficLight implements EventHandler {
   // CONSTRUCTOR
   // ------------------------
   private TrafficLightManager trafficLightManager;
+  private String trafficCondition;
 
-  public TrafficLight(TrafficLightManager trafficLightManager) {
+  public TrafficLight(TrafficLightManager trafficLightManager, String trafficCondidtion) {
     this.trafficLightManager = trafficLightManager;
-
+    this.trafficCondition = trafficCondidtion;
+    
     setStatus(Status.northAndSouthGreenAndArrowLight); // set status to green light for north/south lights
 
     trafficLightManager.addEventHandler(this);
@@ -100,7 +102,6 @@ public class TrafficLight implements EventHandler {
     return wasEventProcessed;
   }
 
-
   @Override
   public boolean moderateTraffic() {
     // TODO Auto-generated method stub
@@ -110,10 +111,9 @@ public class TrafficLight implements EventHandler {
   @Override
   public boolean lowTraffic() {
     boolean wasEventProcessed = false;
-    
+
     Status aStatus = status;
-    switch (aStatus)
-    {
+    switch (aStatus) {
       case northAndSouthGreenAndArrowLight:
         setStatus(Status.northAndSouthGreenLight);
         wasEventProcessed = true;
@@ -147,13 +147,54 @@ public class TrafficLight implements EventHandler {
     return false;
   }
 
-  private void setStatus(Status aStatus)
-  {
+  private void setStatus(Status aStatus) {
     status = aStatus;
 
     // entry actions and do activities
-    switch(status)
-    {
+    switch (status) {
+      // Default case (no traffic)
+      case northAndSouthGreen:
+        // line 8 "model.ump"
+        trafficLightManager.northGreen();
+        // line 9 "model.ump"
+        trafficLightManager.southGreen();
+        // line 10 "model.ump"
+        trafficLightManager.westRed();
+        // line 11 "model.ump"
+        trafficLightManager.eastRed();
+        break;
+      case northAndSouthYellow:
+        // line 16 "model.ump"
+        trafficLightManager.northYellow();
+        // line 17 "model.ump"
+        trafficLightManager.southYellow();
+        // line 18 "model.ump"
+        trafficLightManager.westRed();
+        // line 19 "model.ump"
+        trafficLightManager.eastRed();
+        break;
+      case northAndSouthRed:
+        // line 24 "model.ump"
+        trafficLightManager.northRed();
+        // line 25 "model.ump"
+        trafficLightManager.southRed();
+        // line 26 "model.ump"
+        trafficLightManager.westGreen();
+        // line 27 "model.ump"
+        trafficLightManager.eastGreen();
+        break;
+      case westAndEastYellow:
+        // line 32 "model.ump"
+        trafficLightManager.northRed();
+        // line 33 "model.ump"
+        trafficLightManager.southRed();
+        // line 34 "model.ump"
+        trafficLightManager.westYellow();
+        // line 35 "model.ump"
+        trafficLightManager.eastYellow();
+        break;
+
+      // light case  
       case northAndSouthGreenAndArrowLight:
         trafficLightManager.northArrow();
         trafficLightManager.southArrow();
@@ -186,7 +227,6 @@ public class TrafficLight implements EventHandler {
         break;
     }
   }
-
 
   public void delete() {
   }
